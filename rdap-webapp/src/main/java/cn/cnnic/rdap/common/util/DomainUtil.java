20a32883200e9962c70f5775e2302c77bfd55717
forumbyr <jiashuo@cnnic.cn>
2014-05-30 18:51:51 +0800

    Revert "invalid uri end with '/.' return 400."
    This reverts commit a1b2a9f3e25c7310a5be6f9e5281d9b7b097c439.

--- rdap-webapp/src/main/java/cn/cnnic/rdap/common/util/DomainUtil.java
+++ rdap-webapp/src/main/java/cn/cnnic/rdap/common/util/DomainUtil.java
@@ -194,21 +194,15 @@ public final class DomainUtil {
             LOGGER.error("generate puny name error:" + e.getMessage());
             return false;
         }
-        String punyWithoutLastPoint = deleteLastPoint(punyDomainName);
-        if (!validateDomainLength(punyWithoutLastPoint)) {
-            return false;
-        }
-        String domainNameWithoutLastPoint = deleteLastPoint(domainName);
-        String[] splits = StringUtils.split(punyWithoutLastPoint, ".");
+        String domainWithoutLastPoint = deleteLastPoint(punyDomainName);
+        String[] splits = StringUtils.split(domainWithoutLastPoint, ".");
         if (splits.length > MAX_DOMAIN_LABEL) {
             return false;
         }
-        if (domainName.equals(punyDomainName)
-                || domainNameWithoutLastPoint.equals(punyWithoutLastPoint)) {
-            // all ASCII lable
+        if (domainName.equals(punyDomainName)) {//all ASCII lable
             if (isLdh(domainName)) {
                 return true;
-            } else {
+            }else{
                 return false;
             }
         }
