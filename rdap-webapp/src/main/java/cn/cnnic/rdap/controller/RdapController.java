0f6f8b1df401123c1e85ac5746988e2054015626
forumbyr <jiashuo@cnnic.cn>
2014-07-03 10:06:03 +0800

    fix bug: domain/ns NFKC problem

--- rdap-webapp/src/main/java/cn/cnnic/rdap/controller/RdapController.java
+++ rdap-webapp/src/main/java/cn/cnnic/rdap/controller/RdapController.java
@@ -268,10 +268,10 @@ public class RdapController {
         } catch (Exception e) {
             return RestResponseUtil.createResponse400();
         }
+        decodeDomain = StringUtil.getNormalization(decodeDomain);
         if (!DomainUtil.validateDomainNameIsValidIdna(decodeDomain)) {
             return RestResponseUtil.createResponse400();
         }
-        decodeDomain = StringUtil.getNormalization(decodeDomain);
         LOGGER.debug("after normalization: {}",decodeDomain);
         decodeDomain = DomainUtil.deleteLastPoint(decodeDomain);
         decodeDomain = StringUtils.lowerCase(decodeDomain);
@@ -353,13 +353,13 @@ public class RdapController {
         if (StringUtils.isBlank(decodeDomain)) {
             return RestResponseUtil.createResponse400();
         }
+        decodeDomain = StringUtil.getNormalization(decodeDomain);
         if (!StringUtil.checkIsValidSearchPattern(decodeDomain)) {
             return RestResponseUtil.createResponse422();
         }
         if (!DomainUtil.validateSearchStringIsValidIdna(decodeDomain)) {
             return RestResponseUtil.createResponse400();
         }
-        decodeDomain = StringUtil.getNormalization(decodeDomain);
         decodeDomain = DomainUtil.deleteLastPoint(decodeDomain);
         decodeDomain = StringUtils.lowerCase(decodeDomain);
         DomainSearch domainSearch = searchService.searchDomain(queryParser
@@ -398,10 +398,10 @@ public class RdapController {
         } catch (Exception e) {
             return RestResponseUtil.createResponse400();
         }
+        decodeNS = StringUtil.getNormalization(decodeNS);
         if (!DomainUtil.validateDomainNameIsValidIdna(decodeNS)) {
             return RestResponseUtil.createResponse400();
         }
-        decodeNS = StringUtil.getNormalization(decodeNS);
         LOGGER.debug("after normalization: {}",decodeNS);
         decodeNS = DomainUtil.deleteLastPoint(decodeNS);
         decodeNS = StringUtils.lowerCase(decodeNS);
@@ -486,13 +486,13 @@ public class RdapController {
             if (StringUtils.isBlank(decodeNameserver)) {
                 return RestResponseUtil.createResponse400();
             }
+            decodeNameserver = StringUtil.getNormalization(decodeNameserver);
             if (!StringUtil.checkIsValidSearchPattern(decodeNameserver)) {
                 return RestResponseUtil.createResponse422();
             }
             if (!DomainUtil.validateSearchStringIsValidIdna(decodeNameserver)) {
                 return RestResponseUtil.createResponse400();
             }
-            decodeNameserver = StringUtil.getNormalization(decodeNameserver);
             decodeNameserver = DomainUtil.deleteLastPoint(decodeNameserver);
             decodeNameserver = StringUtils.lowerCase(decodeNameserver);
             nsQueryParam = (NameserverQueryParam) queryParser
