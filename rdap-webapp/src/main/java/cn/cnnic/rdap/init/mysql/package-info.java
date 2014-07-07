da0d8175e8d20f5b452e7733b4b4cd651fde560c
wade <wjk1012@sina.cn>
2014-07-07 11:58:01 +0800

    add packageinfo for package

--- rdap-webapp/src/main/java/cn/cnnic/rdap/init/mysql/package-info.java
+++ rdap-webapp/src/main/java/cn/cnnic/rdap/init/mysql/package-info.java
@@ -28,10 +28,8 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
  * DAMAGE.
  */
-
 /**
- * init mysql.
+ * models.
  * @author jiashuo
- *
  */
 package cn.cnnic.rdap.init.mysql;
