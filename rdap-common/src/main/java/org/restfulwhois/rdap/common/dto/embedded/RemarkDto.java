31e9d344a40994e8921fabb940832421af3bf279
forumbyr <jiashuo@cnnic.cn>
2015-01-19 19:59:32 +0800

    add dto.

--- rdap-common/src/main/java/org/restfulwhois/rdap/common/dto/embedded/RemarkDto.java
+++ rdap-common/src/main/java/org/restfulwhois/rdap/common/dto/embedded/RemarkDto.java
@@ -2,16 +2,42 @@ package org.restfulwhois.rdap.common.dto.embedded;
 
 import java.util.List;
 
-public class RemarkDto{
-	private List<String> description;
+public class RemarkDto {
+    /**
+     * title.
+     */
+    private String title;
+    /**
+     * description.
+     */
+    private List<String> description;
+    /**
+     * links.
+     */
+    private List<LinkDto> links;
 
-	public List<String> getDescription() {
-		return description;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setDescription(List<String> description) {
-		this.description = description;
-	}
-	
-	
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getDescription() {
+        return description;
+    }
+
+    public void setDescription(List<String> description) {
+        this.description = description;
+    }
+
+    public List<LinkDto> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<LinkDto> links) {
+        this.links = links;
+    }
+
 }
