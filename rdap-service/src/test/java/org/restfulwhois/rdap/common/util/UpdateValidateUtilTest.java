4f50099c6ed4281c6c3b1d1fbbacf9b772fca46e
luciferxcv <miaodi1985@126.com>
2015-01-23 12:26:49 +0800

    Add test code.

--- rdap-service/src/test/java/org/restfulwhois/rdap/common/util/UpdateValidateUtilTest.java
+++ rdap-service/src/test/java/org/restfulwhois/rdap/common/util/UpdateValidateUtilTest.java
@@ -1,10 +1,9 @@
 package org.restfulwhois.rdap.common.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.junit.Test;
 import org.restfulwhois.rdap.common.validation.ValidationResult;
@@ -33,6 +32,51 @@ public class UpdateValidateUtilTest {
         ValidationResult validationResult = new ValidationResult();
         UpdateValidateUtil.checkNotEmptyAndValidMinMaxDate(
                 "0001-01-01T00:00:00Z", "eventDate", validationResult);
+        assertTrue(validationResult.hasError());
+    }
+    
+    @Test
+    public void test_checkNotEmptyAndValidMinMaxDate_maxdate_noerror() {
+    	ValidationResult validationResult = new ValidationResult();
+        UpdateValidateUtil.checkNotEmptyAndValidMinMaxDate(
+                "9999-12-31T23:59:59Z", "eventDate", validationResult);
+        assertFalse(validationResult.hasError());
+    }
+    
+    @Test
+    public void test_checkNotEmptyAndValidMinMaxDate_exceed_maxdate_haserror() {
+    	ValidationResult validationResult = new ValidationResult();
+        UpdateValidateUtil.checkNotEmptyAndValidMinMaxDate(
+                "10000-01-01T00:00:00Z", "eventDate", validationResult);
+        assertTrue(validationResult.hasError());
+    }
+    
+    @Test
+    public void test_checkNotEmptyAndValidMinMaxDate_normal_date_norror() {
+    	ValidationResult validationResult = new ValidationResult();
+        UpdateValidateUtil.checkNotEmptyAndValidMinMaxDate(
+                "2015-01-28T00:00:00Z", "eventDate", validationResult);
+        assertFalse(validationResult.hasError());
+    }
+    
+    @Test
+    public void test_checkNotEmptyAndValidMinMaxDate_impossible_date_hasrror() {
+    	ValidationResult validationResult = new ValidationResult();
+        UpdateValidateUtil.checkNotEmptyAndValidMinMaxDate(
+                "2015-25-80T00:00:00Z", "eventDate", validationResult);
+        assertTrue(validationResult.hasError());
+    }
+    
+    @Test
+    public void test_checkNotEmptyAndValidMinMaxDate_empty_date_hasrror() {
+    	ValidationResult validationResult = new ValidationResult();
+        UpdateValidateUtil.checkNotEmptyAndValidMinMaxDate(
+                "", "eventDate", validationResult);
+        assertTrue(validationResult.hasError());
+        
+        validationResult = new ValidationResult();
+        UpdateValidateUtil.checkNotEmptyAndValidMinMaxDate(
+                null, "eventDate", validationResult);
         assertTrue(validationResult.hasError());
     }
 }
