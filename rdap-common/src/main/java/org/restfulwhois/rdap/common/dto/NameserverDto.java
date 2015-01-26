fd07fbf1e96d23e98d909c223ab05ebf7155ea09
forumbyr <jiashuo@cnnic.cn>
2015-01-26 09:43:55 +0800

    refactor: extract public properties to baseDto.

--- rdap-common/src/main/java/org/restfulwhois/rdap/common/dto/NameserverDto.java
+++ rdap-common/src/main/java/org/restfulwhois/rdap/common/dto/NameserverDto.java
@@ -2,24 +2,33 @@ package org.restfulwhois.rdap.common.dto;
 
 import org.restfulwhois.rdap.common.dto.embedded.IpAddressDto;
 
+public class NameserverDto extends BaseDto {
+    private String ldhName;
+    private String unicodeName;
+    private IpAddressDto ipAddresses;
 
+    public String getLdhName() {
+        return ldhName;
+    }
 
-public class NameserverDto extends BaseDto{
-	private String nsName;
-	private IpAddressDto ipAddresses;
-	
-	public String getNsName() {
-		return nsName;
-	}
-	public void setNsName(String nsName) {
-		this.nsName = nsName;
-	}
-	public IpAddressDto getIpAddresses() {
-		return ipAddresses;
-	}
-	public void setIpAddresses(IpAddressDto ipAddresses) {
-		this.ipAddresses = ipAddresses;
-	}
-	
-	
+    public void setLdhName(String ldhName) {
+        this.ldhName = ldhName;
+    }
+
+    public String getUnicodeName() {
+        return unicodeName;
+    }
+
+    public void setUnicodeName(String unicodeName) {
+        this.unicodeName = unicodeName;
+    }
+
+    public IpAddressDto getIpAddresses() {
+        return ipAddresses;
+    }
+
+    public void setIpAddresses(IpAddressDto ipAddresses) {
+        this.ipAddresses = ipAddresses;
+    }
+
 }
